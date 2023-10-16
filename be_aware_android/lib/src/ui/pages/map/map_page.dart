f2f8770ca9c58a9426cb21313075b4d211871868
maciekdt <74464525+maciekdt@users.noreply.github.com>
2023-10-17 00:17:37 +0200

    open street map init

--- be_aware_android/lib/src/ui/pages/map/map_page.dart
+++ be_aware_android/lib/src/ui/pages/map/map_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_osm_plugin/flutter_osm_plugin.dart';
 
 class MapPage extends StatefulWidget {
   const MapPage({Key? key}) : super(key: key);
@@ -7,16 +8,46 @@ class MapPage extends StatefulWidget {
   createState() => _MapPageState();
 }
 
-class _MapPageState extends State<MapPage> {
+class _MapPageState extends State<MapPage> with OSMMixinObserver {
+  late final MapController controller = MapController.withUserPosition(
+      trackUserLocation: UserTrackingOption(
+    enableTracking: true,
+    unFollowUser: false,
+  ));
+
+  @override
+  void initState() {
+    super.initState();
+    controller.addObserver(this);
+  }
+
+  final Key key = GlobalKey();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('BeAware'),
       ),
-      body: const Center(
-        child: Text('Map'),
+      body: Center(
+        child: OSMFlutter(
+          key: key,
+          controller: controller,
+          osmOption: const OSMOption(
+            userTrackingOption: UserTrackingOption(
+              enableTracking: true,
+              unFollowUser: false,
+            ),
+            zoomOption: ZoomOption(
+              initZoom: 13,
+            ),
+          ),
+        ),
       ),
     );
+  }
+
+  @override
+  Future<void> mapIsReady(bool isReady) {
+    return Future<void>.value();
   }
 }
